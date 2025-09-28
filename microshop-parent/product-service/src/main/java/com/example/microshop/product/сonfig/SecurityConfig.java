0c5998c21c697277a8bbce140cab1ac0c31f83d2
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2025-09-29 01:24:40 +0300

    add Keycloak to product-service

---
+++ microshop-parent/product-service/src/main/java/com/example/microshop/product/сonfig/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.microshop.product.сonfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+
+        return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+
+        jwtConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            // Берем объект realm_access
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess == null || !realmAccess.containsKey("roles")) {
+                return List.<GrantedAuthority>of();
+            }
+
+            // Получаем список ролей и превращаем в SimpleGrantedAuthority
+            List<String> roles = (List<String>) realmAccess.get("roles");
+            return roles.stream()
+                    .map(SimpleGrantedAuthority::new) // роли как есть: "ROLE_USER", "ROLE_ADMIN"
+                    .collect(Collectors.toList());
+        });
+
+        return jwtConverter;
+    }
+}
